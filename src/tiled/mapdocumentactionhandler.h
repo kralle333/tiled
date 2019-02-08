95dbe022978475caa0996a4b43bd5275cd927f72
Kristian Pilegaard Jensen <kralle@gmail.com>
2019-02-08 12:03:26 +0100

    Added ability to toggle lock+visibility for a single layer New icons

--- src/tiled/mapdocumentactionhandler.h
+++ src/tiled/mapdocumentactionhandler.h
@@ -84,6 +84,8 @@ public:
     QAction *actionMoveLayerDown() const { return mActionMoveLayerDown; }
     QAction *actionToggleOtherLayers() const { return mActionToggleOtherLayers; }
     QAction *actionToggleLockOtherLayers() const { return mActionToggleLockOtherLayers; }
+    QAction *actionToggleLayer() const { return mActionToggleLayer; }
+    QAction *actionLockLayer() const { return mActionLockLayer; }
     QAction *actionAllowedTilesets() const { return mActionTilesetsAllowed; }
     QAction *actionLayerProperties() const { return mActionLayerProperties; }
 
@@ -132,6 +134,8 @@ public slots:
     void toggleOtherLayers();
     void tilesetsAllowed();
     void toggleLockOtherLayers();
+    void toggleLayer();
+    void lockLayer();
     void layerProperties();
 
     void duplicateObjects();
@@ -170,6 +174,8 @@ private:
     QAction *mActionMoveLayerDown;
     QAction *mActionToggleOtherLayers;
     QAction *mActionToggleLockOtherLayers;
+    QAction *mActionToggleLayer;
+    QAction *mActionLockLayer;
     QAction *mActionTilesetsAllowed;
     QAction *mActionLayerProperties;
 
